5d41151adc32923cbdc66aa2c9a2ec483eac0970
Dmitriy <xyzmail@yandex.ru>
2021-06-11 21:35:48 +0400

    connect allure

---
+++ src/test/java/gitSearch/SelenideTest.java
@@ -0,0 +1,31 @@
+package gitSearch;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+
+    @BeforeAll
+    public static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void testIssueSearch() {
+
+        open("https://github.com");
+        $(".header-search-input").setValue("eroshenkoam/allure-example").pressEnter();
+        $(By.linkText("eroshenkoam/allure-example")).click();
+        $(withText("Iissues")).click();
+        $(withText("#68")).should(exist);
+
+        sleep(3000);
+
+    }
+}
